4923f5ad2ad2f4f94be1f6b3b83687d055e55a99
tadavarthi-rishi <rtadavar@asu.edu>
2024-11-06 16:49:50 -0700

    Added text input and send button for user interaction

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+def clear_input_field():
+    st.session_state.user_question = st.session_state.user_input
+    st.session_state.user_input = ""
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+
+def main():
+
+    st.title("Multimodel Local ChatGptClone")
+    chat_container = st.container()
+
+    if "send_input" not in st.session_state:
+        st.session_state.send_input = False
+        st.session_state.user_question = ""
+
+    user_input = st.text_input("Type your message here", key="user_input", on_change=set_send_input)
+
+    send_button = st.button("Send", key="send_button") 
+    
+
+    if send_button or st.session_state.send_input:
+        if st.session_state.user_question != "":
+            
+            llm_response = "This is the response from the LLM model"
+
+            with chat_container:
+                st.chat_message("User").write(st.session_state.user_question)
+                st.chat_message("Ai").write("here is an answer")
+
+if __name__ == "__main__":
+    main()
